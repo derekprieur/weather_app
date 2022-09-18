0741ce0c299dc4c1989f30bb6baef4535c576711
derekprieur <DPrieur123@gmail.com>
2022-09-18 10:46:11 -0400

    initial project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  RapidAPIKeyName: 'X-RapidAPI-Key',
+  RapidAPIKeyValue: '7be5a9bcc5msh2b74fb5480cd279p17f9aajsn79b848f2237d',
+  RapidAPIHostName: 'X-RapidAPI-Host',
+  RapidAPIHostValue: 'weatherapi-com.p.rapidapi.com'
+
 };
 
 /*
