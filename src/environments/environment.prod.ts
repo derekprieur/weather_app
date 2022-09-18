78f50dea573a4114455624cc4dd85a9d2df9d8eb
derekprieur <DPrieur123@gmail.com>
2022-09-18 11:10:21 -0400

    attempting to fix cors error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  RapidAPIKeyName: 'X-RapidAPI-Key',
+  RapidAPIKeyValue: '7be5a9bcc5msh2b74fb5480cd279p17f9aajsn79b848f2237d',
+  RapidAPIHostName: 'X-RapidAPI-Host',
+  RapidAPIHostValue: 'weatherapi-com.p.rapidapi.com'
 };
